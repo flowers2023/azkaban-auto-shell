c3be701101868bfdec519be8f30563df3706135f
shuangfu <shuangfu.zhang@xiaoi.com>
2017-01-16 17:24:35 +0800

    first commit

--- azkaban.sh
+++ azkaban.sh
@@ -15,32 +15,32 @@ function __init__(){
   session_ID=`echo $json |cut -d':' -f3 |cut -d'"' -f2`
   path=`pwd`
   projectName=${path##*/}
-	echo -e "projectName:"${projectName}
-	echo -e "session_ID:"${session_ID}
+  echo -e "projectName:"${projectName}
+  echo -e "session_ID:"${session_ID}
 }
 
 __init__
 
 function create(){
-	echo -e "\n============start to create project============="
+  echo -e "\n============start to create project============="
   name=$1
-	description=$2
+  description=$2
   curl -s -k -X POST --data "session.id="${session_ID}"&name="${name}"&description="${description} ${url}/manager?action=create
 }
 
 function update(){
-	echo -e "\n============start to zip file to ${projectName}.zip ============="
-	zip ${projectName}.zip -r ./
-	echo -e "\n============start to update project============="
+  cho -e "\n============start to zip file to ${projectName}.zip ============="
+  zip ${projectName}.zip -r ./
+  echo -e "\n============start to update project============="
   name=$1
   curl -k -i -H "Content-Type: multipart/mixed" -X POST  \
-			--form 'session.id='${session_ID} \
-			--form 'ajax=upload'  \
-			--form 'file=@'${name}'.zip;type=application/zip'  \
-			--form 'project='${name}';type=text/plain'  \
-			${url}/manager
-	if [ -n "${projectName}.zip" ]; then
-			rm ${projectName}.zip
+       --form 'session.id='${session_ID} \
+       --form 'ajax=upload'  \
+       --form 'file=@'${name}'.zip;type=application/zip'  \
+       --form 'project='${name}';type=text/plain'  \
+       ${url}/manager
+  if [ -n "${projectName}.zip" ]; then
+    rm ${projectName}.zip
   fi
 }
 
